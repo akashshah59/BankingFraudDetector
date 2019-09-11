1bd90c5025e58c4f7943e8b4ee25735383604475
Akash Shah <akashshah59@gmail.com>
2019-09-11 04:31:41 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask,jsonify,request
 from flask_cors import CORS
-from graph import get_paths,getCustomers,getTargets,getAll
+from path_finder import get_paths,getCustomers,getTargets,getAll
 
 app = Flask(__name__)
 CORS(app)
